86308f753762eaa9ad4fe245880bbd32df071b50
Rrochin <160145881+Rrochin@users.noreply.github.com>
2024-02-20 20:34:01 -0700

    unidad 2: ejercicio 14

--- src/Login.java
+++ src/Login.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -10,6 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+
 public class Login extends JFrame{
 
 	//constructor que define atributos de la ventana
@@ -17,11 +17,12 @@ public class Login extends JFrame{
 		
 		this.setTitle("asd");
 		this.setVisible (true);
-		this.setSize(200,200);
+		this.setSize(800,600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Mi ventana");
 		this.setMinimumSize(new Dimension(250,250));
 		this.setMaximumSize(new Dimension(750,750));
+		this.setOpacity(1);
 		this.setResizable(true);
 		this.setLocation(200,200);
 		this.setLocationRelativeTo(null);
@@ -33,56 +34,55 @@ public class Login extends JFrame{
 	public void iniciarComponentes() {
 		
 		//panel izquierdo
-		JPanel login = new JPanel();
-		login.setSize(this.getWidth()/2, this.getHeight());
-		login.setBackground(Color.red);
-		login.setLayout(null);
-		
-		//panel derecho
-		JPanel registro = new JPanel();
-		registro.setSize(this.getWidth()/2, this.getHeight());
-		registro.setLocation(500,0);
-		registro.setBackground(Color.green);
+		JPanel Login = new JPanel();
+		Login.setSize(this.getWidth()/2, this.getHeight());
+		Login.setBackground(Color.pink);
+		Login.setLayout(null);
 		
 		//boton de acceder
-		JLabel login_tag = new JLabel("Acceder, 0");
-		login_tag.setSize(300,80);
-		login_tag.setFont(new Font("Arial", Font.BOLD, 40));
-		login_tag.setLocation(200,20);
+		JLabel login_tag = new JLabel("Acceder", 0);
+		login_tag.setSize(160,30);
+		login_tag.setFont(new Font("Roboto", Font.BOLD, 20));
+		login_tag.setLocation(315,350);
 		login_tag.setOpaque(true);
-		login_tag.setBackground(Color.red);
-		login.add(login_tag);
+		login_tag.setBackground(Color.white);
+		Login.add(login_tag);
 	
+		//panel derecho
+		//JPanel registro = new JPanel();
+		//registro.setSize(this.getWidth(), this.getHeight());
+		//registro.setLocation(500,0);
+		//registro.setBackground(Color.LIGHT_GRAY);
+
 		//usuario
 		JLabel user_tag = new JLabel("Nombre de usuario: ");
-		user_tag.setBounds(10,120,200,40);
-		login_tag.setFont(new Font("Arial", Font.BOLD, 20));
-		login.add(user_tag);
+		user_tag.setBounds(250,155,200,40);
+		user_tag.setFont(new Font("Arial", Font.BOLD, 15));
+		Login.add(user_tag);
 		
 		JTextField usr_field = new JTextField();
-		usr_field.setBounds(10, 160, 300, 40);
-		login.add(usr_field);
+		usr_field.setBounds(250, 190, 300, 40);
+		Login.add(usr_field);
 		
 		//contraseña
-		//JLabel usr_field = new JLabel("Nombre de usuario: ");
-		//user_tag.setBounds(10,120,200,40);
-		//login_tag.setFont(new Font("Arial", Font.BOLD, 20));
-		//login.add(user_tag);
+		JLabel pwd_tag = new JLabel("Contraseña: ");
+		pwd_tag.setBounds(250,230,200,40);
+		pwd_tag.setFont(new Font("Arial", Font.BOLD, 15));
+		Login.add(pwd_tag);
 		
-		//JPasswordField pwd_field = new JPasswordField();
-		//pwd_field.setBounds(10, 260, 300, 40);
-		//login.add(pwd_field);
+		JPasswordField pwd_field = new JPasswordField();
+		pwd_field.setBounds(250, 260, 300, 40);
+		Login.add(pwd_field);
 		
-		//JCheckBox remember = new JCheckbox("Recordarme");
-		//remember.setBounds(10, 300, 150, 40);
-		//login.add(remember);
-		
-		
-		
+		JCheckBox remember = new JCheckBox("Recordarme");
+		remember.setBounds(250, 300, 150, 40);
+		remember.setOpaque(false);
+		Login.add(remember);
 		
 		
-		this.add(login);
-		this.add(registro);	
+		
+		this.add(Login);
+		//this.add(registro);	
 		
 		}
 	}
--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,12 @@
 
-import java.awt.Dimension;
-
 import javax.swing.JFrame;
 
 public class Main {
 	
 	public static void main(String[] args) {
-			
-		
-JFrame Login = new JFrame();
+			Login l1 = new Login();
 
-Login.setVisible (true);
-
-Login.setSize(200,200);
-		
-Login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-Login.setTitle("Mi ventana");
-
-Login.setMinimumSize(new Dimension(250,250));
-Login.setMaximumSize(new Dimension(750,750));
-Login.setResizable(true);
-
-//Ubicar en la pantalla
-Login.setLocation(200,200);
-
-//centrar
-Login.setLocationRelativeTo(null);
+				l1.setVisible (true);
+  
 }
 }
